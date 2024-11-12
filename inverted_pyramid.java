e5b78ddc9445bd47548aeed45ee5129d799a6f44
Nishant Jha <168707776+Nishant7621@users.noreply.github.com>
2024-11-12 19:47:41 +0530

    Add files via upload

---
+++ inverted_pyramid.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ class Dcoder
+ {
+   public static void main(String args[])
+   { 
+     int n=4;
+     for(int i=n; i>=1; i--)
+     {
+       for(int j=1; j<=i; j++)
+     {
+    System.out.print("*");
+    }
+    System.out.println();
+   }
+   
+ }
+ }
