7991f61261e216d831c1c058c42ef5544c343be3
Nishant Jha <168707776+Nishant7621@users.noreply.github.com>
2024-11-12 19:37:03 +0530

    Add files via upload

---
+++ ascending_order.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class Main
+{  
+  public static void main(String args[])
+  {  
+      int a[]=new int[5];
+      int temp;
+      Scanner sc = new Scanner(System.in);
+    System.out.println("enter array elements");
+      for(int i=0;i<5;i++)
+      {
+          a[i]=sc.nextInt();
+      }
+      for(int i=0;i<5;i++)
+      {
+          for(int j=i+1;j<5;j++)
+          {
+              if(a[i]>a[j])
+              {
+                  temp=a[i];
+                  a[i]=a[j];
+                  a[j]=temp;
+              }
+              }
+          }
+  for(int i=0;i<5;i++)   
+  {
+      System.out.println(a[i]+" ");
+  }
+  
+      
+  }
+}
